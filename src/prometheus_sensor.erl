b4411fd97188aecfa258e7305713eea83ab96c54
Phil Hagelberg <technomancy@gmail.com>
2014-01-04 11:51:49 -0800

    Write temperature and timestamp to log file every minute.

--- src/prometheus_sensor.erl
+++ src/prometheus_sensor.erl
@@ -25,8 +25,11 @@ temperature(Sensor) ->
         _ -> erlang:error(no_sensor)
     end.
 
-log_temp(_Temperature, _Log) ->
-    ok. % TODO: write this
+seconds({MegaSecs,Secs,_MicroSecs}) -> (MegaSecs * 1000000 + Secs).
+
+log_temp(Temperature, LogFile) ->
+    Entry = io_lib:format("~p ~.2f~n", [seconds(erlang:now()), Temperature]),
+    ok = file:write(LogFile, Entry).
 
 
 
@@ -36,11 +39,12 @@ init([]) ->
     erlang:register(prometheus_sensor, self()),
     {ok, Sensor} = application:get_env(prometheus, sensor),
     {ok, Log} = application:get_env(prometheus, log),
+    LogFile = file:open(Log, [append]),
     Temp = temperature(Sensor),
-    {ok, {Temp, Sensor, Log}, ?TIMEOUT}.
+    {ok, {Temp, Sensor, LogFile}, ?TIMEOUT}.
 
-handle_call(temperature, _From, {Temp, Sensor, Log}) ->
-    {reply, Temp, {Temp, Sensor, Log}, ?TIMEOUT};
+handle_call(temperature, _From, {Temp, Sensor, LogFile}) ->
+    {reply, Temp, {Temp, Sensor, LogFile}, ?TIMEOUT};
 handle_call(Message, From, State) ->
     io:format("unexpected handle_call: ~p ~p", [Message, From]),
     {noreply, State, ?TIMEOUT}.
@@ -49,16 +53,17 @@ handle_cast(Message, State) ->
     io:format("unexpected handle_cast: ~p", [Message]),
     {noreply, State, ?TIMEOUT}.
 
-handle_info(timeout, {_Temp, Sensor, Log}) ->
+handle_info(timeout, {_Temp, Sensor, LogFile}) ->
     Temp = temperature(Sensor),
-    log_temp(Temp, Log),
-    {noreply, {Temp, Sensor, Log}, ?TIMEOUT};
+    log_temp(Temp, LogFile),
+    {noreply, {Temp, Sensor, LogFile}, ?TIMEOUT};
 handle_info(Message, State) ->
     io:format("unexpected handle_info: ~p", [Message]),
     {noreply, State, ?TIMEOUT}.
 
-terminate(Reason, _State) ->
+terminate(Reason, {_Temp, _Sensor, LogFile}) ->
     io:format("terminated: ~p", [Reason]),
+    ok = file:close(LogFile),
     ok.
 
 code_change(_PreviousVersion, State, _Extra) ->
